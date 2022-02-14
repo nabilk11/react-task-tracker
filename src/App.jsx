77afbbb37c223309f78170e88c164e9ced5c6303
nabilk11 <khan.nabiln11@gmail.com>
2022-02-13 20:10:50 -0500

    created header component, and cleaned up unneeded files and code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Header } from './components/Header';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="components">
+        <Header title='Hello'/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Header = (props) => {
+  return (
+    <header>
+        <h1>Task Tracker {props.title}</h1>
+
+    </header>
+
+
+  )
+}
+
+
+
+
